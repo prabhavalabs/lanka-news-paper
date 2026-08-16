17593d94502826c21dbd1db3703a15050bc4234d
Nipun Theekshana <theekshana2@gmail.com>
2026-08-16 11:47:44 +0200

    feat(api): poll feeds and refresh the daily brief

--- services/api/internal/jobs/river.go
+++ services/api/internal/jobs/river.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/riverqueue/river"
 	"github.com/riverqueue/river/riverdriver/riverpgxv5"
 	"github.com/riverqueue/river/rivermigrate"
+
+	"github.com/nipuntheekshana/lanka-news-paper/services/api/internal/ingest"
+	"github.com/nipuntheekshana/lanka-news-paper/services/api/internal/publish"
 )
 
 type PollArgs struct{}
@@ -18,10 +22,36 @@ func (PollArgs) Kind() string { return "ingest.poll" }
 
 type PollWorker struct {
 	river.WorkerDefaults[PollArgs]
+	Poller *ingest.Poller
+	News   *publish.Store
 }
 
-func (worker *PollWorker) Work(_ context.Context, _ *river.Job[PollArgs]) error {
+func (worker *PollWorker) Work(ctx context.Context, _ *river.Job[PollArgs]) error {
+	if worker.Poller != nil {
+		if err := worker.Poller.PollAll(ctx); err != nil {
+			return err
+		}
+	}
+	if worker.News != nil {
+		return worker.News.WriteBrief(ctx)
+	}
 	return nil
+}
+
+type BriefArgs struct{}
+
+func (BriefArgs) Kind() string { return "brief.daily" }
+
+type BriefWorker struct {
+	river.WorkerDefaults[BriefArgs]
+	News *publish.Store
+}
+
+func (worker *BriefWorker) Work(ctx context.Context, _ *river.Job[BriefArgs]) error {
+	if worker.News == nil {
+		return nil
+	}
+	return worker.News.WriteBrief(ctx)
 }
 
 func Migrate(ctx context.Context, pool *pgxpool.Pool) error {
@@ -35,12 +65,21 @@ func Migrate(ctx context.Context, pool *pgxpool.Pool) error {
 	return nil
 }
 
-func NewClient(pool *pgxpool.Pool, logger *slog.Logger) (*river.Client[pgx.Tx], error) {
+func NewClient(pool *pgxpool.Pool, logger *slog.Logger, poller *ingest.Poller, news *publish.Store) (*river.Client[pgx.Tx], error) {
 	workers := river.NewWorkers()
-	river.AddWorker(workers, &PollWorker{})
+	river.AddWorker(workers, &PollWorker{Poller: poller, News: news})
+	river.AddWorker(workers, &BriefWorker{News: news})
 
 	client, err := river.NewClient(riverpgxv5.New(pool), &river.Config{
 		Logger: logger,
+		PeriodicJobs: []*river.PeriodicJob{
+			river.NewPeriodicJob(river.PeriodicInterval(time.Minute), func() (river.JobArgs, *river.InsertOpts) {
+				return PollArgs{}, nil
+			}, &river.PeriodicJobOpts{RunOnStart: true}),
+			river.NewPeriodicJob(river.PeriodicInterval(time.Hour), func() (river.JobArgs, *river.InsertOpts) {
+				return BriefArgs{}, nil
+			}, nil),
+		},
 		Queues: map[string]river.QueueConfig{
 			river.QueueDefault: {MaxWorkers: 2},
 		},
